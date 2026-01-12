1dbf614489ce523920a7fd85fd3099a0962c9cc6
dterrero <dicksonterrero1@hotmail.com>
2026-01-12 17:13:44 -0500

    Update cnn_hd2.py

--- cnn_hd2.py
+++ cnn_hd2.py
@@ -4,18 +4,21 @@ from urllib.parse import urlparse, parse_qs
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    page.goto("https://tvpass.org/channel/cnn-us")
 
-    # Wait for the .m3u8 request to appear
     def handle_request(request):
         if ".m3u8" in request.url:
             qs = parse_qs(urlparse(request.url).query)
-            print("token:", qs.get("token", [None])[0])
-            print("user_id:", qs.get("user_id", [None])[0])
-            print("expires:", qs.get("expires", [None])[0])
+            token = qs.get("token", [None])[0]
+            user_id = qs.get("user_id", [None])[0]
+            expires = qs.get("expires", [None])[0]
+
+            # Only print if we actually got the values
+            if token and user_id and expires:
+                print("token:", token)
+                print("user_id:", user_id)
+                print("expires:", expires)
 
     page.on("request", handle_request)
-
-    # Let page load for a few seconds
-    page.wait_for_timeout(5000)
+    page.goto("https://tvpass.org/channel/cnn-us")
+    page.wait_for_timeout(5000)  # wait a few seconds for requests to fire
     browser.close()
