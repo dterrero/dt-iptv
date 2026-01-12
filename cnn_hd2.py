baee4fe649b9bb616a7a0562198bf3be9dfe4d8c
dterrero <dicksonterrero1@hotmail.com>
2026-01-12 16:38:21 -0500

    Refactor cnn_hd2.py to eliminate requests usage
    Removed requests dependency and simplified query parameter extraction.

--- cnn_hd2.py
+++ cnn_hd2.py
@@ -1,19 +1,9 @@
-import requests
 from urllib.parse import urlparse, parse_qs
 
 url = "https://tvpass.org/channel/cnn-us"
 
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Referer": "https://tvpass.org/",
-}
+qs = parse_qs(urlparse(url).query)
 
-r = requests.get(url, headers=headers)
-
-# Extract query parameters
-qs = parse_qs(urlparse(r.url).query)
-
-print("token:", qs.get("token"))
-print("user_id:", qs.get("user_id"))
-print("expires:", qs.get("expires"))
-
+print(qs["token"][0])
+print(qs["user_id"][0])
+print(qs["expires"][0])
